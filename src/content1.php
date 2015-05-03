0523112a4a78915f8f44467516fa565f9ea22486
Yizheng Wang <wangyizh@onid.oregonstate.edu>
2015-05-03 14:36:25 +0800

    Revert "debug"
    This reverts commit 43ca56cfcf3d8d8ee54daab69b48eaf221b06379.

--- src/content1.php
+++ src/content1.php
@@ -1,72 +1,63 @@
-<?php	
-session_start();
-?>
-<?php
-		if ($_GET['logout']==1){
-			session_destroy();
-			header('Location:login.php');
-			die();
-		}
-?>
-<?php
-	if(isset($_POST['username'])  &&  $_SESSION['log']!=1){
-		$_SESSION['logz'] = 1;
-	}
-?>
-<?php
-	if ($_GET['logout1']==1){
-		session_destroy();
-		header('Location:login.php');
-		die();
-	}
-?>
-<?php
-		if ($_SESSION['logz'] != 1  && $_SESSION['log']!=1){
-				header('Location:login.php');
-		}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8" />
 <title>content1</title>
+
 </head>
 <body>
 	<?php 
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-		//if (!empty($_POST['username']) || $_SESSION['log']==1){
-		if(session_status() == PHP_SESSION_ACTIVE && $_SESSION['logz']==1){
+		session_start();
 		if(isset($_POST['username'])){
-			if (!empty($_POST['username'])){
+			$_SESSION['log'] = 1;
+		}
+		if($_SESSION['log'] != 1){
+			$_SESSION = array();
+			session_destroy();
+			header('Location: login.php');
+			die();
+		}
+		if(session_status() == PHP_SESSION_ACTIVE){
+			if (!empty($_POST['username']) || $_SESSION['log']==1){
+			if(isset($_POST['username'])){
 				$_SESSION['username'] = $_POST['username'];
-				if(!isset($_SESSION['visits'])){
-					$_SESSION['visits'] = 0;
-				}
-				$_SESSION['logz']=0;
-				$_SESSION['log']=1;
-			}else{
+			}
+			if ($_SESSION['log'] != 1){
+				header('Location:login.php');
+			}
+			if(!isset($_SESSION['visits'])){
+				$_SESSION['visits'] = 0;
+			}
 
+			$_SESSION['visits']++;
+			$foo1 = $_SESSION['visits']-1;
+			echo "Hi $_SESSION[username], you have visited this page $foo1 times before.Click ";
+
+			echo '<a href="?logout=1" >here</a>';
+
+			echo ' to logout.';
+
+			echo '<p><a href="content2.php" target="_self">link to content2.php</a>';
+			if ($_GET['logout']==1){
+				session_destroy();
+				header('Location:login.php');
+				die();
+			}
+			}else{
+				if ($_GET['logout']==1){
+				session_destroy();
+				header('Location:login.php');
+				die();
+				}
 				echo "A username must be entered.Click ";
 
-				echo '<a href="?logout1=1" >here</a>';
+				echo '<a href="?logout=1" >here</a>';
 
 				echo ' to return to the login screen.';
+
 			}
-		}
-	}	
-	if (session_status() == PHP_SESSION_ACTIVE && $_SESSION['log']==1){
-		$_SESSION['visits']++;
-		$foo1 = $_SESSION['visits']-1;
-		echo "Hi $_SESSION[username], you have visited this page $foo1 times before.Click ";
-
-		echo '<a href="?logout=1" >here</a>';
-
-		echo ' to logout.';
-
-		echo '<p><a href="content2.php" target="_self">link to content2.php</a>';
-		}
 			
+		}
 	?>
 </body>
 </html>
--- src/content2.php
+++ src/content2.php
@@ -1,28 +1,19 @@
-<?php		
-session_start();
-?>
-<?php
-if($_SESSION['log'] != 1){
-			//$_SESSION = array();
-			session_destroy();
-			header("Location:  login.php",true);
-			die();
-		}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8" />
-<title>content1</title>
+<title>content2</title>
 </head>
 <body>
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-		if(session_status() == PHP_SESSION_ACTIVE){
+	<?php
+		session_start();
+		if($_SESSION['log'] != 1){
+			$_SESSION = array();
+			session_destroy();
+			header('Location: login.php');
+			die();
+		}
 		echo '<p><a href="content1.php" target="_self">link to content1.php</a>';
-	}
-?>
+	?>
 </body>
 </html>
-
